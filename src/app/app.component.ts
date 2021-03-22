adaf228edfaaf50350f08c7540776223debc2f88
Vitor Piovezam <vitorpiovezam@yandex.com>
2021-03-22 16:24:08 -0300

    Projeto criado, servico https injetado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inmetrics-angular';
+  constructor(private http: HttpClient) {
+    console.log(this.http);
+  }
+
+  list = [];
+  number = 1;
+  title = 'inmetrics';
+
+  add(number: number) {
+    this.number += number
+  }
+
+  request() {
+    this.http.get('https://my-blog-cms.herokuapp.com/posts')
+    .subscribe(x => this.list = x as []);
+  }
 }
